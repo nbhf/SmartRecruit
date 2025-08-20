17f55e8784366e79819b73093b3b8e927014793a
nbhf <houdabhf123@gmail.com>
2025-08-20 16:16:18 +0100

    Navbar css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,9 +8,10 @@ return(
 
     <nav className="navbar">
         <ul>
+          <li><strong><Link to="/">SmartRecruit</Link></strong></li>
           <li><Link to="/">Job Offers List</Link></li>
-          <li><Link to="/upload">Upload New Cv</Link></li>
           <li><Link to="/addoffer">Add New Job Offer</Link></li>
+          <li><Link to="/upload">Upload New Cv</Link></li>
         </ul>
       </nav>
     
